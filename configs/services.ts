be24911eeee9d16f029837f164501a9cd2804775
Ganesh K <ganeshk.work@gmail.com>
2023-02-12 13:01:42 +0530

    Add cloudinary for image upload

---
+++ configs/services.ts
@@ -0,0 +1,11 @@
+export declare type cloudinaryConfig = {
+  cloud_name: string;
+  api_key: string;
+  api_secret: string;
+};
+
+export const cloudinaryConfig: cloudinaryConfig = {
+  cloud_name: 'dih1r5web',
+  api_key: '417548972434439',
+  api_secret: 'e7pnFSkZGBydMTGjU9xpicqYn9Q',
+};
