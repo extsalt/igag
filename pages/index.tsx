03cdc152ad9132d15de4e063cce655ba974e1a2b
Ganesh K <45989209+extsalt@users.noreply.github.com>
2023-02-15 06:36:35 +0530

    Update index.tsx (#16)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from '@next/font/google';
-import { Button } from '@chakra-ui/react';
+import { Button, Skeleton, Stack } from '@chakra-ui/react';
 import Post from '@/components/posts/post';
 import useSWR from 'swr';
 import { POST_GET_URL } from '@/configs/apis';
@@ -10,7 +10,14 @@ export default function Home() {
   const { data, error, isLoading } = useSWR(POST_GET_URL, fetcher);
 
   if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
+  if (isLoading)
+    return (
+      <Stack>
+        <Skeleton height="20px" />
+        <Skeleton height="20px" />
+        <Skeleton height="20px" />
+      </Stack>
+    );
 
   return data.map((post: any) => <Post post={post} key={post.id} />);
 }
