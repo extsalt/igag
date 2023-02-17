d787da83ac86c557a49ee3d2597df7c7d2de3caa
Ganesh K <ganeshk.work@gmail.com>
2023-02-17 08:00:26 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { Skeleton, Stack } from '@chakra-ui/react';
 import { POST_GET_URL } from '@/configs/apis';
 import useSWRInfinite from 'swr/infinite';
+import { useEffect, useRef } from 'react';
+import Post from '@/components/posts/post';
 
 const options = {
   initialSize: 1,
   revalidateAll: false,
 };
+
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 const getKey = (_pageIndex: any, _previousPageData: any) => {
@@ -14,26 +17,35 @@ const getKey = (_pageIndex: any, _previousPageData: any) => {
 
 export default function Home() {
   const { data, size, setSize } = useSWRInfinite(getKey, fetcher, options);
+  const sentinel: any = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          console.log('Oh my bottom');
+        }
+      });
+    });
+
+    observer.observe(sentinel.current);
+  }, []);
+
   return (
-    <div>
-      {getPost(data)}
-      <button
-        onClick={() => setSize(size + 1)}
-        style={{ border: '1px solid blue' }}
-      >
-        Load more...
-      </button>
-    </div>
+    <>
+      <div>{getPost(data)}</div>
+      <div ref={sentinel} style={{ marginTop: '30px' }}>
+        sentinel
+      </div>
+    </>
   );
 }
 
-function getPost(posts: any[] | undefined) {
-  console.log(posts);
-
+function getPost(posts: any[] | undefined): JSX.Element {
   if (posts) {
     const pagePost = posts[0];
     return pagePost.map(function (post: any) {
-      return <div key={post.id}> {post.title}</div>;
+      return <Post key={post.id} post={post} />;
     });
   }
 
