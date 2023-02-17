aafc97dbfa6d0de8534f602546939901444db21c
Ganesh K <45989209+extsalt@users.noreply.github.com>
2023-02-17 08:03:15 +0530

    Feat infinite scroll (#18)
    * Update index.tsx (#16)
    
    Update index.tsx
    
    * Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,53 @@
-import { Button, Box, Container, Skeleton, Stack } from '@chakra-ui/react';
+import { Skeleton, Stack } from '@chakra-ui/react';
+import { POST_GET_URL } from '@/configs/apis';
+import useSWRInfinite from 'swr/infinite';
+import { useEffect, useRef } from 'react';
 import Post from '@/components/posts/post';
-import usePost from '@/lib/effects/post';
-export default function Home() {
-  const { posts, error, isLoading } = usePost();
 
-  if (error) return <div>failed to load</div>;
-  if (isLoading)
-    return (
-      <Stack>
-        <Skeleton height="20px" />
-        <Skeleton height="20px" />
-        <Skeleton height="20px" />
-      </Stack>
-    );
+const options = {
+  initialSize: 1,
+  revalidateAll: false,
+};
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+const getKey = (_pageIndex: any, _previousPageData: any) => {
+  return POST_GET_URL;
+};
+
+export default function Home() {
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher, options);
+  const sentinel: any = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          console.log('Oh my bottom');
+        }
+      });
+    });
+
+    observer.observe(sentinel.current);
+  }, []);
 
   return (
-    <Container as="main" maxW="lg">
-      <Button>Call Effect</Button>
-      {posts.map((post: any) => (
-        <Post post={post} key={post.id} />
-      ))}
-
-      <Box>
-        <p>Loading more...</p>
-      </Box>
-    </Container>
+    <>
+      <div>{getPost(data)}</div>
+      <div ref={sentinel} style={{ marginTop: '30px' }}>
+        sentinel
+      </div>
+    </>
   );
+}
+
+function getPost(posts: any[] | undefined): JSX.Element {
+  if (posts) {
+    const pagePost = posts[0];
+    return pagePost.map(function (post: any) {
+      return <Post key={post.id} post={post} />;
+    });
+  }
+
+  return <></>;
 }
