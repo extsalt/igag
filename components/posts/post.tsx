338096913e95a73d8f3a06643198e78f3f193163
Ganesh K <ganeshk.work@gmail.com>
2023-02-23 10:21:11 +0530

    Update post.tsx

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -3,16 +3,36 @@ export default function Post({ post }: any) {
     <>
       <div className="my-2">
         {/* post header */}
-        <div className="flex justify-between">
-          <div>hello</div>
-          <div>world</div>
+        <div className="flex justify-between flex-1 border">
+          {/* left side */}
+          <div className="p-2 bg-white flex items-center space-x-4 ">
+            <div className="">
+              <img
+                className="h-12 w-12 rounded-full"
+                src={post.imageUrl}
+                alt={post.title}
+              />
+            </div>
+            <div>
+              <div className="text-xl font-medium text-black">
+                CybedafsdfasdfasdrPunk
+              </div>
+              <p className="text-slate-500">2 days ago</p>
+            </div>
+          </div>
+          {/* left side */}
+
+          {/* right side */}
+          <div className="p-4">...</div>
+          {/* right side */}
         </div>
         {/* post header */}
 
-        <div>
-          {/* post body */}
+        {/* post body */}
+        <div className="text-center">
           <img className="object-fill" src={post.imageUrl} alt={post.title} />
         </div>
+        {/* post body */}
 
         <div>{/* post footer */}</div>
       </div>
