82430afd4fb319f18a5f4ba5bb46a495debe5e04
Ganesh K <ganeshk.work@gmail.com>
2023-02-23 15:20:50 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 export default function Page404() {
   return (
     <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
@@ -10,12 +11,12 @@ export default function Page404() {
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
+          <Link
             href="/"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
+          </Link>
         </div>
       </div>
     </main>
