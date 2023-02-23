cd1b792cd22b62d1bfb512dde4a8dd70dcac4543
Ganesh K <ganeshk.work@gmail.com>
2023-02-23 19:36:48 +0530

    Add other identity provider

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,6 @@
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
+import Link from 'next/link';
 export default function Login() {
   return (
     <div className="bg-gray-200 min-w-full min-h-screen py-10">
@@ -9,7 +10,7 @@ export default function Login() {
         </div>
 
         <div className="my-4 flex justify-center space-x-4">
-          <button onClick={() => signIn('google')}>
+          <button className="" onClick={() => signIn('google')}>
             <Image
               src="/google.svg"
               alt="Login with google"
@@ -25,37 +26,68 @@ export default function Login() {
               height={24}
             />
           </button>
+
+          <button onClick={() => signIn('github')}>
+            <Image
+              src="/github.svg"
+              alt="Login with github"
+              width={24}
+              height={24}
+            />
+          </button>
+
+          <button onClick={() => signIn('facebook')}>
+            <Image
+              src="/facebook.svg"
+              alt="Login with facebook"
+              width={24}
+              height={24}
+            />
+          </button>
         </div>
 
         <div className="form-control my-4">
           <div className="flex flex-col">
-            <label htmlFor="email" className="">
-              Your email
+            <label htmlFor="email" className="font-semibold">
+              Email
             </label>
             <input
-              className="border-2 border-blue-300 px-2 py-1 rounded-sm"
-              type="text"
+              className="border-2 border-gray-300 px-2 py-1 rounded-sm"
+              type="email"
               id="email"
-              placeholder="Email"
             />
           </div>
 
-          <div className="flex flex-col my-2">
-            <label htmlFor="password" className="">
-              Your password
-            </label>
+          <div className="flex flex-col my-4">
+            <div className="flex justify-between">
+              <label htmlFor="password" className="font-semibold">
+                Password
+              </label>
+
+              <Link href="/forgot-password" className="text-blue-600">
+                Forgot password?
+              </Link>
+            </div>
             <input
-              className="border-2 border-blue-300 px-2 py-1 rounded-sm"
+              className="border-2 border-gray-300 px-2 py-1 rounded-sm"
               type="password"
               id="password"
-              placeholder="Password"
             />
           </div>
           <div className="my-2 flex justify-center">
-            <button className="px-4 py-1 bg-blue-600 text-white rounded-sm">
+            <button className="px-6 py-2 bg-blue-600 min-w-full text-white rounded-sm">
               Login
             </button>
           </div>
+        </div>
+      </div>
+
+      <div className="max-w-sm mx-auto my-6">
+        <div className="flex justify-center space-x-2">
+          <span>Don't have an account? </span>
+          <Link href="/register" className="text-blue-600">
+            Sign up
+          </Link>
         </div>
       </div>
     </div>
