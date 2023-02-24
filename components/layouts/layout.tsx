8faa5cecf9a5595a0e46955f06349f1d48115572
Ganesh K <45989209+extsalt@users.noreply.github.com>
2023-02-24 06:19:26 +0530

    Add other identity provider (#27)

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -119,8 +119,6 @@ export default function Layout({ children }: any) {
               <button
                 type="button"
                 className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
                 onClick={() => {
                   setShowMobileNav(!showMobileNav);
                 }}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,6 @@
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
+import Link from 'next/link';
 export default function Login() {
   return (
     <div className="bg-gray-200 min-w-full min-h-screen py-10">
@@ -47,33 +48,46 @@ export default function Login() {
 
         <div className="form-control my-4">
           <div className="flex flex-col">
-            <label htmlFor="email" className="">
-              Your email
+            <label htmlFor="email" className="font-semibold">
+              Email
             </label>
             <input
-              className="border-2 border-blue-300 px-2 py-1 rounded-sm"
+              className="border-2 border-gray-300 px-2 py-1 rounded-sm"
               type="email"
               id="email"
-              placeholder="Email"
             />
           </div>
 
-          <div className="flex flex-col my-2">
-            <label htmlFor="password" className="">
-              Your password
-            </label>
+          <div className="flex flex-col my-4">
+            <div className="flex justify-between">
+              <label htmlFor="password" className="font-semibold">
+                Password
+              </label>
+
+              <Link href="/forgot-password" className="text-blue-600">
+                Forgot password?
+              </Link>
+            </div>
             <input
-              className="border-2 border-blue-300 px-2 py-1 rounded-sm"
+              className="border-2 border-gray-300 px-2 py-1 rounded-sm"
               type="password"
               id="password"
-              placeholder="Password"
             />
           </div>
           <div className="my-2 flex justify-center">
-            <button className="px-4 py-1 bg-blue-600 text-white rounded-sm">
+            <button className="px-6 py-2 bg-blue-600 min-w-full text-white rounded-sm">
               Login
             </button>
           </div>
+        </div>
+      </div>
+
+      <div className="max-w-sm mx-auto my-6">
+        <div className="flex justify-center space-x-2">
+          <span>Don't have an account? </span>
+          <Link href="/register" className="text-blue-600">
+            Sign up
+          </Link>
         </div>
       </div>
     </div>
