4a59bec9c13d1ceaa9d412d83391799243ea91b7
Ganesh K <45989209+extsalt@users.noreply.github.com>
2023-02-28 08:48:40 +0530

    Add oauth user (#38)

--- configs/urls.ts
+++ configs/urls.ts
@@ -2,3 +2,4 @@ import { server } from './index';
 
 export const OAUTH_SIGN_IN_REDIRECT_URL = server + '/users/oauth/';
 export const USER_CREATE_URL = server + '/api/users/store';
+export const USER_OAUTH_CREATE_URL = server + '/api/users/oauth/store';
