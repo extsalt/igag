af4b6b70bf12f8bf2ce5d53e7427521c62731e34
Ganesh K <45989209+extsalt@users.noreply.github.com>
2023-03-05 10:16:59 +0530

    Add post title (#39)

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -1,28 +1,29 @@
 import EllipsisVertical from '../icons/ellipsis-vertical';
+
 export default function Post({ post }: any) {
   return (
     <>
-      <div className="my-2 min-w-full">
+      <div className='my-2 min-w-full'>
         {/* post header */}
-        <div className="flex justify-between flex-1 border rounded-sm">
+        <div className='flex justify-between flex-1 border rounded-sm'>
           {/* left side */}
-          <div className="p-2 bg-white flex items-center space-x-4">
-            <div className="object-center">
+          <div className='p-2 bg-white flex items-center space-x-4'>
+            <div className='object-center'>
               <img
-                className="h-12 w-12 rounded-full object-contain"
+                className='h-12 w-12 rounded-full object-contain'
                 src={post.imageUrl}
                 alt={post.title}
               />
             </div>
             <div>
-              <div className="text-lg font-medium">Cyberpunk</div>
-              <p className="text-slate-500 text-sm">2 days ago</p>
+              <div className='text-lg font-medium'>Cyberpunk</div>
+              <p className='text-slate-500 text-sm'>2 days ago</p>
             </div>
           </div>
           {/* left side */}
 
           {/* right side */}
-          <div className=" px-4 flex justify-center">
+          <div className=' px-4 flex justify-center'>
             <button>
               <EllipsisVertical />
             </button>
@@ -31,19 +32,32 @@ export default function Post({ post }: any) {
         </div>
         {/* post header */}
 
+
+        {/*Post title*/}
+        <div className='px-2 py-2'>
+          {post.title}
+        </div>
+        {/*Post title*/}
+
+
         {/* post body */}
-        <div className="post-media-container">
+        <div className='post-media-container'>
           <img
-            className="h-post-image-height min-w-full object-cover object-center rounded-sm"
+            className='h-post-image-height min-w-full object-cover object-center rounded-sm'
             src={post.imageUrl}
             alt={post.title}
-            loading="lazy"
-            decoding="async"
+            loading='lazy'
+            decoding='async'
           />
         </div>
         {/* post body */}
 
-        <div>{/* post footer */}</div>
+        
+        {/* post footer */}
+        <div></div>
+        {/* post footer */}
+
+
       </div>
     </>
   );
